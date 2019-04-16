d5a6e9fed417c1fc46dd4e945cc762cbc6a545ab
Fpromiss <1326372778@qq.com>
2019-04-16 21:04:40 +0800

    [feat] 测试接入mybatis

--- miaosha/src/main/java/com/miaoshaproject/App.java
+++ miaosha/src/main/java/com/miaoshaproject/App.java
@@ -1,22 +1,36 @@
 package com.miaoshaproject;
 
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * Hello world!
  *
  */
-@EnableAutoConfiguration // app启动类设置为springboot的bean并且能够开启整个工程的自动配置
+// app启动类设置为springboot的bean并且能够开启整个工程的自动配置 并且 配置扫描包
+@SpringBootApplication(scanBasePackages = {"com.miaoshaproject"})
 @RestController // 设置springMVC controller
+@MapperScan("com.miaoshaproject.dao")
 public class App 
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     // 访问根路径 输出hello world
     @RequestMapping("/")
     public String home(){
-        return "hello world";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null){
+            return "用户对象不存在";
+        }else{
+            return userDO.getName();
+        }
     }
 
     public static void main( String[] args )
