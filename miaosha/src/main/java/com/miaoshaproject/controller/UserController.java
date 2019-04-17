c9432c1933bae6d015bab74e931fb56f38cafcd1
Fpromiss <1326372778@qq.com>
2019-04-17 13:53:39 +0800

    [fix] 通过VO解决不必要字段传入前端

--- miaosha/src/main/java/com/miaoshaproject/controller/UserController.java
+++ miaosha/src/main/java/com/miaoshaproject/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.miaoshaproject.controller;
 
+import com.miaoshaproject.controller.viewobject.UserVO;
 import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.UserModel;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +21,26 @@ public class UserController {
 
     @RequestMapping("/get")
     @ResponseBody
-    public UserModel getUser(@RequestParam(name = "id")Integer id){
+    public UserVO getUser(@RequestParam(name = "id")Integer id){
         // 调用service服务获取对应id的用户对象并返回给前端
         UserModel userModel = userService.getUserById(id);
-        return userModel;
+        // 将核心领域模型用户对象转换为可供UI使用的viewobject
+        return convertFromModel(userModel);
+    }
+
+    /**
+     * 将 userModel 转换为 userVO
+     *
+     * @param userModel
+     * @return
+     */
+    private UserVO convertFromModel(UserModel userModel){
+        if(userModel == null){
+            return null;
+        }
+        UserVO userVO = new UserVO();
+        BeanUtils.copyProperties(userModel, userVO);
+        return userVO;
     }
 
 }
