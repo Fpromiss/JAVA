30b891aab6de03442cded6713456a3d594a05abd
Fpromiss <1326372778@qq.com>
2019-04-17 19:28:36 +0800

    [feat] 定义通用的返回对象--返回错误信息

--- miaosha/README.md
+++ miaosha/README.md
@@ -11,3 +11,13 @@
 （3）前端model VO（view object）
 
 即前端所需要展示字段集合，避免如密码等透露到前端
+
+二、commonReturnType
+
+（1）是通用返回对象，包含状态码和数据对象
+
+```
+// 若status=success 则data返回前端需要的json数据
+// 若status=fail 则data内使用通用的错误码格式
+```
+
---
+++ miaosha/src/main/java/com/miaoshaproject/error/CommonError.java
@@ -0,0 +1,9 @@
+package com.miaoshaproject.error;
+
+public interface CommonError {
+    public int getErrorCode();
+
+    public String getErrorMsg();
+
+    public CommonError setErrorMsg(String errorMsg);
+}
