cfb49aaed3df03de946dca900e6d63598357ebbe
Fpromiss <1326372778@qq.com>
2019-05-30 21:34:25 +0800

    [feat] 编译器

---
+++ Compiler/src/com/compiler/model/AddressCode.java
@@ -0,0 +1,39 @@
+package com.compiler.model;
+
+/**
+ * ÈýµØÖ·ÂëÀà
+ */
+public class AddressCode {
+
+	private String code;
+	private String place, line;
+
+	public AddressCode() {
+
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public String getLine() {
+		return line;
+	}
+
+	public void setLine(String line) {
+		this.line = line;
+	}
+
+}
---
+++ Compiler/src/com/compiler/util/PropertiesUtil.java
@@ -0,0 +1,28 @@
+package com.compiler.util;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * 读入语法、分隔符处理
+ */
+public class PropertiesUtil {
+	public static Map<String, String> getPropertiesMap(String path) {
+		Properties p = new Properties();
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			p.load(ClassLoader.getSystemResourceAsStream(path));
+			for (Object key : p.keySet()) {
+				map.put(key.toString(), p.getProperty(key.toString()));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return map;
+	}
+}
