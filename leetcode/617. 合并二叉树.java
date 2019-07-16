afb67d1f9de7d9f0a00c6d69c9f49b65c87e93fa
Fpromiss <1326372778@qq.com>
2019-07-16 22:33:12 +0800

    [feat] 617. 合并二叉树

---
+++ leetcode/617. 合并二叉树.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/** 
+ * 题目描述：
+ *
+ * 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+ *
+ * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为?NULL 的节点将直接作为新二叉树的节点。
+ *
+ * 思路：
+ *（1）递归
+ *（2）广度优先
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null){
+            return t2;
+        }
+        if(t2 == null){
+            return t1;
+        }
+        TreeNode result = new TreeNode(t1.val + t2.val);
+        result.left = mergeTrees(t1.left, t2.left);
+        result.right = mergeTrees(t1.right, t2.right);
+        return result;
+    }
+}
